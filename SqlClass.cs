6d32a80ec6df1663c978833b2a8832ab4ce5ba8f
Enes Özçelik <36846516+mnsozcelik@users.noreply.github.com>
2020-05-20 02:06:01 +0300

    SQL class created and configured for Local Database connection.

---
+++ SqlClass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace pc_toplama_sistemi
+{
+    public class SqlClass
+    {
+        public SqlConnection Connection()
+        {
+            SqlConnection connect = new SqlConnection(@"DESKTOP-CS4BDGI\SQLEXPRESS;Initial Catalog=pcbuild;Integrated Security=True");
+            connect.Open();
+            return connect;
+        }
+    }
+}
